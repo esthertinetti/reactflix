656893cfdd4169c9a98b4d5844a7968c888e5198
Esther Tinetti <esthertinetti@gmail.com>
2020-07-28 18:43:27 -0300

    frontend projeto

--- src/pages/Cadastro/Categoria/index.js
+++ src/pages/Cadastro/Categoria/index.js
@@ -6,6 +6,16 @@ function CadastroCategoria() {
   return (
     <PageDefaul>
       <h1>Cadastro de Categoria</h1>
+
+      <form>
+        <label>
+          Nome da Categoria:
+          <input type="text" />
+        </label>
+
+        <button>Cadastrar</button>
+      </form>
+
       <Link to="/"> 
         Ir pra home
       </Link>
